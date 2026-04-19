68a397c3267d7273ebbba16691ae2a33032c3b65
WillemPreterre <willem.preterre@gmail.com>
2026-04-19 14:30:43 +0200

    logo et router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import Navbar from "./components/Navbar";
 import "./App.scss";
-import Main from "./pages/Main/Main.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import Navbar from "./components/NavBar/Navbar.js";
 import Footer from "./components/Footer/Footer.js";
-
+import Main from "./pages/Main/Main.js";
+import Projects from "./pages/Projects/Projects.js";
+import ProjectDetail from "./pages/Projects/ProjectDetail.js";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GoogleReCaptchaProvider
         reCaptchaKey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
       >
         <Navbar />
-        <main>
-          <Main />
-        </main>
-
+        <Routes>
+          <Route path="/"                  element={<main><Main /></main>} />
+          <Route path="/projects/:slug"    element={<ProjectDetail />}     />
+        </Routes>
         <Footer />
       </GoogleReCaptchaProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
